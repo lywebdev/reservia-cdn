650b8313daa98122fa0fe9a9d014ecc46cc42890
Kulik Leonid <lyonyauei@gmail.com>
2025-03-16 18:09:50 +0300

    first commit

---
+++ reserviaKernel.js
@@ -0,0 +1,35 @@
+(function () {
+    const $reserviaWidgetsContainer = document.createElement('div');
+    $reserviaWidgetsContainer.id = '.reserviaWidgets';
+    document.body.appendChild($reserviaWidgetsContainer);
+
+    const loadWidget = async () => {
+        const shadow = $reserviaWidgetsContainer.attachShadow({ mode: 'open' });
+        const $shadowContainer = document.createElement('div');
+        $shadowContainer.id = 'shadow-container';
+        shadow.appendChild($shadowContainer);
+
+        const style = document.createElement('style');
+        shadow.appendChild(style);
+
+        const allowed = true;
+        if (!allowed) {
+            console.log('Widget not authorized for this domain');
+            return;
+        }
+
+        const script = document.createElement('script');
+        // script.src = 'reserviaBookingWidget.js';
+        script.src = 'build/widgets/reserviaBookingWidget.js';
+        script.async = 1;
+        script.type = "module";
+
+        script.onerror = () => {
+            console.error(`Error loading script ${src}`);
+        };
+
+        shadow.appendChild(script);
+    };
+
+    loadWidget();
+})();
